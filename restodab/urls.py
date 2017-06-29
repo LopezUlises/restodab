8a619be32e1c391a4803ed83ec363bd0c90d2759
LopezUlises <uliseslopez_11@hotmail.com>
2017-06-29 09:20:45 -0300

    urls

--- restodab/urls.py
+++ restodab/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.post_list),
+        url (r'^bebida/$', views.listar_bebida),
+        url (r'^lanch/$', views.listar_lanch),
 ]
+
+
+
--- restodab/views.py
+++ restodab/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Bebida
+from .models import Lanch
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'pagina1.html', {})
+    bebidasvar = Bebida.objects.all()
+    return render(request, 'pagina1.html', {'bebidas': bebidasvar})
+
+def listar_bebida(request):
+    bebidasvar = Bebida.objects.all()
+    return render(request, 'pagbeb.html', {'bebidas': bebidasvar})
+
+def listar_lanch(request):
+    lanchvar = Lanch.objects.all()
+    return render(request, 'paglan.html', {'lanch': lanchvar})
