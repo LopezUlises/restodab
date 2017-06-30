b03183604f6cf5a3a2c1d7aefe0eced3332cf8af
lopezulises <uliseslopez_11@hotmail.com>
2017-06-30 08:53:06 -0300

    pagina-html

--- restodab/urls.py
+++ restodab/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
         url(r'^$', views.post_list),
         url (r'^bebida/$', views.listar_bebida),
         url (r'^lanch/$', views.listar_lanch),
+        url (r'^postre/$', views.listar_postre),
+        url (r'^guarnicion/$', views.listar_guarnicion),
+        url (r'^breackfast/$', views.listar_breackfast),
 ]
 
 
--- restodab/views.py
+++ restodab/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Bebida
 from .models import Lanch
+from .models import Postre
+from .models import Guarnicion
+from .models import Breackfast
 
 # Create your views here.
 def post_list(request):
@@ -14,3 +17,15 @@ def listar_bebida(request):
 def listar_lanch(request):
     lanchvar = Lanch.objects.all()
     return render(request, 'paglan.html', {'lanch': lanchvar})
+
+def listar_postre(request):
+    postrevar = Postre.objects.all()
+    return render(request, 'pagpostre.html', {'postre': postrevar})
+
+def listar_guarnicion(request):
+    guarnicionvar = Guarnicion.objects.all()
+    return render(request, 'pagguar.html', {'guarnicion': guarnicionvar})
+
+def listar_breackfast(request):
+    breackvar = Breackfast.objects.all()
+    return render(request, 'pagbreack.html', {'breack': breackvar})
