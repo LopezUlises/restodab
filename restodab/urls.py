8edf71ee70bb375094e506a8f60cd79d74dcc418
Thevegit0 <taieltraversa@gmail.com>
2017-07-13 10:35:15 -0300

    modificaciones

--- restodab/urls.py
+++ restodab/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.bebidas_list),
+        url(r'^$', views.pagprincipal),
+        url(r'^bebida/$', views.bebidas_list),
+        url(r'^postre/$', views.postre_list),
+        url(r'^guarnicion/$', views.guarnicion_list),
+        url(r'^lunch/$', views.lunch_list),
+        url(r'^breakfast/$', views.breakfast_list),
 ]
--- restodab/views.py
+++ restodab/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render
 from .models import Consumicion
 
 
+def pagprincipal(request):
+    return render(request, 'pagina1.html')
+
 def guarnicion_list(request):
     convar = Consumicion.objects.all().filter(categoria="G").order_by('nombre')
     return render(request, 'lista.html', {'consumicion': convar})
 
 def bebidas_list(request):
     convar = Consumicion.objects.all().filter(categoria="B").order_by('nombre')
+    return render(request, 'lista.html', {'consumicion': convar})
+
+def postre_list(request):
+    convar = Consumicion.objects.all().filter(categoria="P").order_by('nombre')
+    return render(request, 'lista.html', {'consumicion': convar})
+
+def lunch_list(request):
+    convar = Consumicion.objects.all().filter(categoria="L").order_by('nombre')
+    return render(request, 'lista.html', {'consumicion': convar})
+
+def breakfast_list(request):
+    convar = Consumicion.objects.all().filter(categoria="D").order_by('nombre')
     return render(request, 'lista.html', {'consumicion': convar})
