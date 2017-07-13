a48f924a48170c9b6d28445916688b9fc5934861
Lopezulises <uliseslopez1112@gmail.com>
2017-07-13 11:09:21 -0300

    titulo arreglado

--- restodab/views.py
+++ restodab/views.py
@@ -7,20 +7,26 @@ def pagprincipal(request):
 
 def guarnicion_list(request):
     convar = Consumicion.objects.all().filter(categoria="G").order_by('nombre')
-    return render(request, 'lista.html', {'consumicion': convar})
+    P = "Guarniciones"
+    return render(request, 'lista.html', {'consumicion': convar, 'titulo' : P})
 
 def bebidas_list(request):
     convar = Consumicion.objects.all().filter(categoria="B").order_by('nombre')
-    return render(request, 'lista.html', {'consumicion': convar})
+    P = "Bebidas"
+    return render(request, 'lista.html', {'consumicion': convar, 'titulo' : P})
 
 def postre_list(request):
     convar = Consumicion.objects.all().filter(categoria="P").order_by('nombre')
-    return render(request, 'lista.html', {'consumicion': convar})
+    P = "Postres"
+    return render(request, 'lista.html', {'consumicion': convar, 'titulo' : P})
 
 def lunch_list(request):
     convar = Consumicion.objects.all().filter(categoria="L").order_by('nombre')
-    return render(request, 'lista.html', {'consumicion': convar})
+    P = "Lunch's"
+    return render(request, 'lista.html', {'consumicion': convar, 'titulo' : P})
 
 def breakfast_list(request):
     convar = Consumicion.objects.all().filter(categoria="D").order_by('nombre')
-    return render(request, 'lista.html', {'consumicion': convar})
+    P = "Desayunos"
+    return render(request, 'lista.html', {'consumicion': convar, 'titulo' : P})
+
