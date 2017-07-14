82e26308fa45d798c44dcfb111830934b1ffdab6
Alan Rodas Bonjour <alanrodas@gmail.com>
2017-07-14 07:26:08 -0300

    Correccion 14/07

--- restodab/urls.py
+++ restodab/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.pagprincipal),
-        url(r'^bebida/$', views.bebidas_list),
-        url(r'^postre/$', views.postre_list),
-        url(r'^guarnicion/$', views.guarnicion_list),
-        url(r'^lunch/$', views.lunch_list),
-        url(r'^breakfast/$', views.breakfast_list),
+        url(r'^$', views.pagprincipal, name="restodab.views.pagprincipal"),
+        url(r'^bebida/$', views.bebidas_list, name="restodab.views.bebidas_list"),
+        url(r'^postre/$', views.postre_list, name="restodab.views.postre_list"),
+        url(r'^guarnicion/$', views.guarnicion_list, name="restodab.views.guarnicion_list"),
+        url(r'^lunch/$', views.lunch_list, name="restodab.views.lunch_list"),
+        url(r'^breakfast/$', views.breakfast_list, name="restodab.views.breakfast_list")
 ]
